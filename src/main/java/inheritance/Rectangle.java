fb394f41ad9292d601f8b2de2db4e62c6fc79d86
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-28 18:44:34 +0100

    adding comment

--- src/main/java/inheritance/Rectangle.java
+++ src/main/java/inheritance/Rectangle.java
@@ -5,13 +5,17 @@ public class Rectangle extends Shape {
     private int width;
     private int height;
 
+    // this() or super() in same constructor only can be used one of them! and must be in the first line!
+
     // 1st constructor
     public Rectangle(int x, int y) {
+        // always: this() must be the first line!
         this(x, y, 0, 0); // calls the second constructor
     }
 
     // 2nd constructor
     public Rectangle(int x, int y, int width, int height) {
+        // always: super() must be the first line!
         super(x, y); // calls the constructor from parent (Shape)
         this.width = width;
         this.height = height;
